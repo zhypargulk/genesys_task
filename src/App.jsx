33748cc759979abe2e2d85bcd552058103a63c3c
zhypargulk <zhypargulkubanova@gmail.com>
2023-05-18 03:22:08 +0200

    second commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,12 @@
-import { BrowserRouter as Router, Routes, Route, useLocation } from "react-router-dom";
-import { useEffect } from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import ScrollToTop from "./components/ScrollToTop";
 import HomePage from "./pages/HomePage";
 import ProfilePage from "./pages/ProfilePage";
-
-const ScrollToTop = () => {
-  const { pathname } = useLocation();
-
-  useEffect(() => {
-    window.scrollTo(0, 0);
-  }, [pathname]);
-}
 
 const App = () => {
   return (
     <Router>
+      <ScrollToTop />
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/profile/:id" element={<ProfilePage />} />
